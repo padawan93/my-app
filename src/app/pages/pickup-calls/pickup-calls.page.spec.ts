c2ec6839f7ce682c2f52962030286e5eba03feb2
TGPadawan <saifulashraf.ht@gmail.com>
2022-06-29 15:00:50 +0800

    Kick starter

--- src/app/pages/pickup-calls/pickup-calls.page.spec.ts
+++ src/app/pages/pickup-calls/pickup-calls.page.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { PickupCallsPage } from './pickup-calls.page';
 
 describe('PickupCallsPage', () => {
   let component: PickupCallsPage;
   let fixture: ComponentFixture<PickupCallsPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PickupCallsPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PickupCallsPage);
